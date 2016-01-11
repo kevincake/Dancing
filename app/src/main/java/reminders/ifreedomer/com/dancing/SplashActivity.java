3f5ed2b7be349cc776111a0614e3c494ba3ef7b9
Eava.wu <wuyihua86@gmail.com>
2016-01-12 01:07:27 +0800

    Dancing is great

---
+++ app/src/main/java/reminders/ifreedomer/com/dancing/SplashActivity.java
@@ -0,0 +1,17 @@
+package reminders.ifreedomer.com.dancing;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
