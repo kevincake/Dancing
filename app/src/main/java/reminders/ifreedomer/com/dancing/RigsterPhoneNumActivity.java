d87c6dc5ae74130a5c1616e6f2031650bd46f922
Eava.wu <wuyihua86@gmail.com>
2016-02-02 03:34:29 +0800

    增加提交验证码的Loading条

--- app/src/main/java/reminders/ifreedomer/com/dancing/RigsterPhoneNumActivity.java
+++ app/src/main/java/reminders/ifreedomer/com/dancing/RigsterPhoneNumActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wang.avi.AVLoadingIndicatorView;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,6 +28,7 @@ public class RigsterPhoneNumActivity extends AppCompatActivity implements View.O
     TextView goLoginTv = null;
     Button registerBtn = null;
     EditText verifyCodeEt = null;
+    AVLoadingIndicatorView loadingIndicatorView=null;
     Timer mTimer = new Timer();
     private int sub_time = 60;
     EventHandler eh;
@@ -47,7 +50,7 @@ public class RigsterPhoneNumActivity extends AppCompatActivity implements View.O
 
             @Override
             public void afterEvent(int event, int result, Object data) {
-
+                loadingIndicatorView.setVisibility(View.INVISIBLE);
                 if (result == SMSSDK.RESULT_COMPLETE) {
                     //回调完成
                     if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
@@ -143,6 +146,7 @@ public class RigsterPhoneNumActivity extends AppCompatActivity implements View.O
                 }
 //                Intent intent1 = new Intent(this,FullUserInfoActivity.class);
 //                startActivity(intent1);
+                loadingIndicatorView.setVisibility(View.VISIBLE);
                 SMSSDK.submitVerificationCode(getString(R.string.contry_code), phoneNumEt.getText().toString(), verifyCodeEt.getText().toString());
                 break;
             default:
